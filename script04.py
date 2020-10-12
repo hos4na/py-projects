f7b62dbdcac345784c25af9d93ed131f5313f948
Hosana Barcelos <55986782+hos4na@users.noreply.github.com>
2020-10-12 11:44:54 -0300

    Delete script04.py

--- script04.py
+++
@@ -1,8 +0,0 @@
-nome = input("Digite seu nome: ")
-n1 = float(input("Digite sua nota 1: "))
-n2 = float(input("Digite sua nota 2: "))
-media = n1 + n2
-if media < 7:
-    print('Sua média é {}. REPROVADO!'.format(media))
-elif media > 7:
-    print('Sua média é {}. APROVADO!'.format(media))
